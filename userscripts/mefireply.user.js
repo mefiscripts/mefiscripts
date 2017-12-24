2d0279d6e32b2115274ce8a93681fdad0d6b9ed6
emanuelfeld <emanuelfeld@gmail.com>
2017-12-23 19:13:45 -0500

    Adds new MefiReply userscript

---
+++ userscripts/mefireply.user.js
@@ -0,0 +1,96 @@
+// ==UserScript==
+// @name         MefiReply
+// @namespace    https://github.com/emanuelfeld
+// @version      0.1
+// @description  Adds anchor to quote-reply and link back to original comments
+// @author       Emanuel Feld
+// @include      http://*.metafilter.com/*
+// @include      https://*.metafilter.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let replyArea = document.getElementById('comment');
+    let pageLocation = location.protocol + '//' + location.host + location.pathname;
+
+    let host = location.host;
+    if (host.indexOf('www.') === 0) {
+        host = host.slice(4);
+    }
+
+    let site = host.split('.')[0];
+
+    appendReplyLinks(site);
+
+    // monitor for new comments
+    let observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function (mutationRecord) {
+          let newDiv = document.getElementById('newcommentsmsg');
+          if (newDiv.style.display === 'none') {
+            appendReplyLinks(site);
+          }
+        });
+    });
+
+    let target = document.getElementById('newcommentsmsg');
+    observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+
+    function isComment (element, site) {
+        return (['metafilter', 'projects', 'music', 'irl', 'metatalk'].indexOf(site) > -1 &&
+                    element.id === '' &&
+                    element.querySelector('.whitesmallcopy') === null) ||
+                (['ask', 'fanfare'].indexOf(site) > -1 && element.id.indexOf('c') === 0);
+    }
+
+    // a reply link to each comment
+    function appendReplyLinks (site) {
+        let comments = document.querySelectorAll('div.comments');
+        for (let i = 0; i < comments.length; i++) {
+            if (isComment(comments[i], site)) {
+                let commentMetaLinks = comments[i].querySelectorAll('.smallcopy > a');
+                let commentUsername = commentMetaLinks[0].textContent;
+                let commentAnchor = commentMetaLinks[1].href;
+
+                // skip a link if replying to mod
+                if (commentMetaLinks[1].href.indexOf('faq.metafilter.com') > -1) {
+                  commentAnchor = commentMetaLinks[2].href;
+                }
+
+                let commentId = commentAnchor.split('#')[1];
+
+                if (document.getElementById('mefiReply' + commentId) === null) {
+                    let replySpan = document.createElement('span');
+                    replySpan.id = 'reply' + commentAnchor.split('#')[1];
+                    replySpan.innerHTML = '[<a id="mefiReply' + commentId + '" style="font-weight: normal;" href="' +
+                                            pageLocation + '#comment">reply</a>]';
+                    replySpan.querySelector('a').addEventListener ('click', function () {
+                            addReply(commentUsername, commentId, commentAnchor);
+                          }, false);
+                    comments[i].querySelector('.smallcopy').appendChild(replySpan);
+                 }
+            }
+        }
+    }
+
+    // add text to comment box
+    function addReply (username, id, anchor) {
+        let commentText;
+        try {
+            commentText = document.getElementById('c' + id).textContent;
+        } catch (e) {
+            commentText = document.querySelector('[name="' + id + '"]')
+                                  .nextElementSibling.textContent;
+        }
+       commentText = commentText.split('posted by').slice(0, -1).join().trim();
+
+        let replyText = '<a href="' + anchor + '">> ' + username + ':</a>\r\n<em>"' + commentText + '"</em>\r\n\r\n';
+        replyArea.value = replyArea.value.trim();
+        if (replyArea.value.length !== 0) {
+            replyArea.value += '\r\n\r\n' + replyText;
+        } else {
+            replyArea.value = replyText;
+        }
+    }
+})();
